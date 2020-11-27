75ab7e58707ca85bf5d80a23f8d5409436d283a3
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-11-27 12:29:38 -0500

    Added the scrapy files

--- Data_scraping/scrapy/fx.py
+++ Data_scraping/scrapy/fx.py
@@ -1,5 +1,6 @@
 import scrapy
 from urllib.parse import urlparse
+from scrapy.cmdline import execute
 
 
 class FXSpider(scrapy.Spider):
@@ -24,3 +25,4 @@ class FXSpider(scrapy.Spider):
             ratio = float(price_td.css('::text').get())
             data[symbol] = ratio
         yield data
+
