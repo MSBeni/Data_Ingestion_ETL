bb06fe569de2bd55a1b7994d39fb9ef090379943
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-02-01 15:38:28 -0500

    Added tweeter app 2

--- Free_Data_Resources/tweeter/constants.py
+++ Free_Data_Resources/tweeter/constants.py
@@ -1,6 +1,11 @@
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
+import json
 
-REQUEST_TOKEN_URL = ''
-ACCESS_ToKEN_URL = ''
-AUTHORIZATION_URL = ''
+secrets = json.loads(open('../../../tweeter_app_credential.json', 'r').read())
+print(secrets['auth'])
+
+CONSUMER_KEY = secrets['auth']['api_key']
+CONSUMER_SECRET = secrets['auth']['api_secret']
+
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
