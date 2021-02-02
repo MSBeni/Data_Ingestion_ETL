501b47dc5b0af6a2064e8ac2526cc405339696a1
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-02-02 16:13:34 -0500

    Added section descriptions 2

--- Free_Data_Resources/tweeter/login.py
+++ Free_Data_Resources/tweeter/login.py
@@ -14,18 +14,23 @@ if response.status != 200:
 # Get the request token parsing the query string returned
 requested_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
 
+# Ask the user to authorize our app and
 print("Go to thw following site in your browser...")
 print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, requested_token['oauth_token']))
 
 oauth_verifier = input("What is the TOKEN? ")
 
+# Create the Token object which contains the request token and the verifier
 token = oauth2.Token(requested_token['oauth_token'], requested_token['oauth_token_secret'])
 token.set_verifier(oauth_verifier)
 
-# More known client for twitter
+# Create a client with our consumer (our app) and newly created (and verified) token
 client = oauth2.Client(consumer, token)
+
+# Ask Twitter for an access token, and Twitter knows it should give us it since we've verified the request token
 response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
 access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
 
 print(access_token)
 
+# Create an 'authorized_token' Token object and use that to perfoem Twitter API calls on behalf of the user
