d6478f47aa53b5dfd29415fd6e8a40bda6d56a74
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-02-02 16:04:47 -0500

    Added tweet authorization code 2

--- Free_Data_Resources/tweeter/login.py
+++ Free_Data_Resources/tweeter/login.py
@@ -14,3 +14,16 @@ requested_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
 
 print("Go to thw following site in your browser...")
 print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, requested_token['oauth_token']))
+
+oauth_verifier = input("What is the TOKEN? ")
+
+token = oauth2.Token(requested_token['oauth_token'], requested_token['oauth_token_secret'])
+token.set_verifier(oauth_verifier)
+
+# More known client for twitter
+client = oauth2.Client(consumer, token)
+response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
+access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+print(access_token)
+
