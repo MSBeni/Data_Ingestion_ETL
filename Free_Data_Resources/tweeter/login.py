9be2079ab4ebf75b9ad34d76a62fc895c343ccd5
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-02-02 16:06:15 -0500

    Added section descriptions

--- Free_Data_Resources/tweeter/login.py
+++ Free_Data_Resources/tweeter/login.py
@@ -6,10 +6,12 @@ import urllib.parse as urlparse
 consumer = oauth2.Consumer(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
 client = oauth2.Client(consumer)
 
+# Use the client to perform a request for the request token
 response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
 if response.status != 200:
     print('There is a Problem with the Response Body ...')
 
+# Get the request token parsing the query string returned
 requested_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
 
 print("Go to thw following site in your browser...")
