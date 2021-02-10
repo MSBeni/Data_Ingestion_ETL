7d21d6d1248421da5923f2e49aca93259a0bbdff
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-02-09 19:37:42 -0500

    Added the login to twiiter path to the app and saving the token request data as cookie

--- Free_Data_Resources/tweeter/app.py
+++ Free_Data_Resources/tweeter/app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect
+from Free_Data_Resources.tweeter.tweeter_utils import get_request_token
 
 app = Flask(__name__)
+app.secret_key = '1234'
 
 @app.route('/')
 def hello():
     return render_template('home.html')
+
+
+@app.route('/login/twitter')
+def twitter_login():
+    request_token = get_request_token()
+    session['request_token'] = request_token
+
+    return redirect('http://youtube.com')
+
 
 app.run(port=4495)
 
