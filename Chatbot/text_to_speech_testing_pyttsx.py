f9a66a46cfd144728c0114530b444c298882eaa4
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-04-07 10:06:52 -0400

    Added text to speech 2

--- Chatbot/text_to_speech_testing_pyttsx.py
+++ Chatbot/text_to_speech_testing_pyttsx.py
@@ -1,1 +1,10 @@
-# pip install pyttsx3
+# pip install
+# sudo apt-get update && sudo apt-get install espeak
+import pyttsx3
+
+engine = pyttsx3.init()
+
+engine.say("hello friends how are you? Are you ready for a small game?")
+engine.setProperty('rate', 120)
+engine.setProperty('volume', 0.9)
+engine.runAndWait()
