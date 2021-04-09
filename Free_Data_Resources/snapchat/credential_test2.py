fbba4132ed1aca6c7cc7044f1cb0995e69913bf1
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-04-08 21:26:16 -0400

    Added the snapchat api test 3

--- Free_Data_Resources/snapchat/credential_test2.py
+++ Free_Data_Resources/snapchat/credential_test2.py
@@ -6,6 +6,14 @@ from requests_oauthlib import OAuth1
 import json
 
 
+# export default {
+#   SNAP_ACCOUNTS_AUTH_URL: 'https://accounts.snapchat.com/accounts/oauth2/auth',
+#   SNAP_ACCOUNTS_TOKEN_URL:
+#     'https://accounts.snapchat.com/accounts/oauth2/token',
+#   SNAP_KIT_API_URL: 'https://kit.snapchat.com/v1',
+#   OAUTH_SCOPE_URL_PREFIX: 'https://auth.snapchat.com/oauth2/api/'}
+
+
 secrets = json.loads(open('../../../snapchat_final_test_app.json', 'r').read())
 
 # client_key = ''
