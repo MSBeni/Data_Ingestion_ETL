9918b63462170dc0e986ecde40ffe86ce4896582
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-04-14 17:17:06 -0400

    Added the new query python SDK

---
+++ BigQuery/bigquery_query.py
@@ -0,0 +1,26 @@
+from google.cloud import bigquery
+
+SERVICE_ACCESS_JSON = '/home/i-sip_iot/PycharmProjects/bigquery-demo-308819-96977b1b6c1e.json'
+
+client = bigquery.Client.from_service_account_json(SERVICE_ACCESS_JSON)
+
+query_req = """
+            CREATE TABLE dataset_py.newtable
+            (
+            x INT64 OPTIONS (description="An optional Integer Field"),
+            y STRUCT<
+                a ARRAY<STRING> OPTIONS(description="A repeated STRING field"),
+                b BOOL
+            >
+            )
+            OPTIONS(
+                expiration_timestamp=TIMESTAMP "2023-01-01 00:00:00 UTC",
+                description="a table that expire in 2023",
+                labels=[("org_unit", "development")]
+            )
+"""
+
+query_job = client.query(query_req)
+
+print(query_job)
+print("script ran ...")
