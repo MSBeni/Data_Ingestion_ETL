6084400b053cd2bbc540ffc181fc83d891908490
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-04-14 17:55:14 -0400

    Added apache Beam pipeline mandatory code

--- Apache_Beam_GCP_dataPipeline/beam_pipeline.py
+++ Apache_Beam_GCP_dataPipeline/beam_pipeline.py
@@ -3,9 +3,18 @@ import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
 import argparse
 
+
+# ######################## Mandatory Part to run any Beam pipeline ########################################
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--input', dest='input', required=True, help='input file to process.')
 
 # pipeline_args will hold the environmental args and path_args will hold the import file location
 path_args, pipeline_args = parser.parse_known_args()
+
+
+input_pattern = path_args.input
+options = PipelineOptions(pipeline_args)
+
+p = beam.Pipeline(options=options)
+############################################################################################################
