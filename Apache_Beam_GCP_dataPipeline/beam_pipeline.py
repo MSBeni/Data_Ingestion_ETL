9afcfb2f2421932bc2303c5034ad6ac5739bb0db
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-04-14 17:52:07 -0400

    Added apache Beam Project readme 4

--- Apache_Beam_GCP_dataPipeline/beam_pipeline.py
+++ Apache_Beam_GCP_dataPipeline/beam_pipeline.py
@@ -1,1 +1,11 @@
 import apache_beam as beam
+# import the packages PipelineOptions and StandardOptions for standard configurations like executing the pipeline
+from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--input', dest='input', required=True, help='input file to process.')
+
+# pipeline_args will hold the environmental args and path_args will hold the import file location
+path_args, pipeline_args = parser.parse_known_args()
