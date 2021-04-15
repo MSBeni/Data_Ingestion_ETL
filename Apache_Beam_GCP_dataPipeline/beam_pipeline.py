48bcd6c42e3953840ff9d5167e28eed811ee2050
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-04-14 20:56:27 -0400

    Added apache Beam pipeline mandatory code 2

--- Apache_Beam_GCP_dataPipeline/beam_pipeline.py
+++ Apache_Beam_GCP_dataPipeline/beam_pipeline.py
@@ -18,3 +18,15 @@ options = PipelineOptions(pipeline_args)
 
 p = beam.Pipeline(options=options)
 ############################################################################################################
+# a method which is used in Map transformation. Map basically applies a simple one to one mapping function over each
+# element in the collection. It means it just receive one row and its output is just one row.
+def remove_last_colon(row):
+    pass
+
+# Write Transformation to clean the data in beam
+# p collection is a unified storage of beam that store any batch or streaming data
+cleaned_data = (
+    p
+    | beam.io.ReadFromText(input_pattern, skip_header_lines=1)
+    | beam.Map(remove_last_colon)     # Map applies a simple one to one mapping func over each element in the collection
+)
